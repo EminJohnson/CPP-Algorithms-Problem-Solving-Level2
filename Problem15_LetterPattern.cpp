543e782a28e961466bfb827786b3bb6ae73f92ce
Abdelhamid Med Amine <amine23444@gmail.com>
2026-04-20 23:55:48 +0100

    Add letter pattern program in Problem15_LetterPattern.cpp
    Implement a program to display a letter pattern based on user input.

---
+++ Problem15_LetterPattern.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void GetValidateNumber(unsigned short& Number)
+{
+    while(true)
+    {
+        if(!(cin >> Number))
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Invalid !!, Enter areal Number : \n";
+            continue;
+        }
+
+        if( Number < 1 || Number > 9)
+        {
+            cout << "Enter other Number [1 -> 9] : \n";
+            continue;
+        }
+        break;
+    }
+}
+
+unsigned short ReadNumber()
+{
+    unsigned short Number = 0;
+    cout << "Enter Your Number : \n";
+    GetValidateNumber(Number);
+    return Number;
+}
+
+void NumberPattern(unsigned short Number)
+{
+    for (char i = 65; i <= (65 + Number - 1); i++)
+    {
+        for (unsigned short j = (i - 65 + 1); j >= 1; j--)
+        {
+
+           cout << i;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    NumberPattern(ReadNumber());
+}
