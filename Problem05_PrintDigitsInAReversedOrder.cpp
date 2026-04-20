76a1d73500d6c55a3adbc040a95e7dcbbbaea49c
Abdelhamid Med Amine <amine23444@gmail.com>
2026-04-20 22:03:27 +0100

    Add program to print digits in reversed order

---
+++ Problem05_PrintDigitsInAReversedOrder.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void GetValidateNumber(unsigned short& Number)
+{
+    while(true)
+    {
+        if(!(cin >> Number))
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Invalid !!, Enter areal Number : \n";
+            continue;
+        }
+
+        if( Number < 1 || Number > 9999)
+        {
+            cout << "Enter other Number : \n";
+            continue;
+        }
+        break;
+    }
+}
+
+unsigned short ReadNumber()
+{
+    unsigned short Number = 0;
+    cout << "Enter Your Number : \n";
+    GetValidateNumber(Number);
+    return Number;
+}
+
+void GetDigits(unsigned short Number)
+{
+    unsigned short Remainder = 0;
+    while( Number > 0)
+    {
+        Remainder = Number % 10;
+        Number /= 10;
+        cout << Remainder << endl;
+    }
+
+}
+
+int main()
+{
+    GetDigits(ReadNumber());
+    
+    return 0;
+}
