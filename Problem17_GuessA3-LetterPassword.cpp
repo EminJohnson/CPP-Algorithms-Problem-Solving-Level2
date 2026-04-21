9f4e421af31dbd7cdaebc3e905c569b4a2337267
Abdelhamid Med Amine <amine23444@gmail.com>
2026-04-21 20:47:13 +0100

    Implement 3-letter password guessing game

---
+++ Problem17_GuessA3-LetterPassword.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string ReadPass()
+{
+    string Pass = "";
+    cout << "Enter Your Pass A3 - Letter Password (all capital): \n";
+    getline(cin, Pass);
+    return Pass;
+}
+
+bool GuessPassword(string Pass)
+{
+    string World = "";
+    int Counter = 0;
+    for (char i = 65; i <= 90; i++)
+    {
+        for (char j = 65; j <= 90; j++)
+        {
+            for (char k = 65; k <= 90; k++)
+            {
+
+                    Counter ++;
+                    World += i;
+                    World += j;
+                    World += k;
+
+                    if (Pass == World)
+                    {
+                         cout << "Password is " << World 
+                              << " Found after " << Counter << " Trial (s) \n";
+                        return true;
+                        
+                    }
+                    else
+                    {
+                        cout << "Trial [" << Counter << "] : " << World << endl;
+                        World = "";
+                    }   
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    GuessPassword(ReadPass());  
+    return 0;
+}
